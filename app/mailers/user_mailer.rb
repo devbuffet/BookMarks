58715d33d17f0aa0e920ac4d9ce53bff58708ddc
devbuffet <basharabuelone1@yahoo.com>
2015-05-10 23:00:33 -0400

    changed email proxy

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   include ApplicationHelper
   # this will change once we go live...
-  default from: Rails.application.config.siteUserName
+  default from: ENV['siteUserName']
 
   def signup_sendMail(user, subject_tx)
     @user = user
@@ -11,7 +11,7 @@ class UserMailer < ActionMailer::Base
 
   def passwordreset_sendMail(user, subject_tx)
     @user = user
-     @url_tx = Rails.application.config.url + "users/" + user.id.to_s + "/edit?token=" + user.code_tx.to_s
+    @url_tx = Rails.application.config.url + "users/" + user.id.to_s + "/edit?token=" + user.code_tx.to_s
     mail to: user.email, subject: subject_tx
   end 
 end
